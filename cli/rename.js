7e614cbd838e8c1269062e4cc6d73120bf77c5a2
Gabe Herbert <gh@gabeherbert.com>
2023-10-16 12:43:18 -0700

    makes more name changes

--- cli/rename.js
+++ cli/rename.js
@@ -9,11 +9,13 @@ const phpSedFactory = require( './lib/phpSedFactory');
 /**
  * Rewrite plugin name, slug, function prefix and namespace in php files
  */
-function changeNameInPhpFiles({slug,rootNamespace,pluginName,}){
+function changeNameInPhpFiles({slug,rootNamespace,pluginName,originalNamespace, originalPluginName}){
     let sed = phpSedFactory({slug,rootNamespace})
-    shell.mv( 'wordpress-plugin.php', `${slug}.php` );
+    shell.mv( 'pm2-modern-plugin.php', `${slug}.php` ); // HERE
     sed(`${slug}.php`);
     shell.sed('-i', 'PluginNamespace', pluginName, `${slug}.php`); // HERE
+    shell.sed('-i', 'PLUGIN_NAME', pluginName, `${slug}.php`); // HERE
+    shell.sed('-i', 'VendorNamespace', originalNamespace, `${slug}.php`); // HERE
     shell.ls('**/*.php').forEach(sed);
 }
 
@@ -33,14 +35,17 @@ readline.question(`What is your plugin's slug? Used for translation domain, main
         readline.question(`Plugin name? `, pluginName => {
             readline.question(`Github username? `, githubUserName => {
                 let originalNamespace = 'VendorNamespace'; // HERE
-                changeNameInPhpFiles({slug,rootNamespace,pluginName,originalNamespace});
+                const originalPluginName = 'PLUGIN_NAME'; // HERE
+                changeNameInPhpFiles({slug,rootNamespace,pluginName,originalNamespace, originalPluginName});
                 // changeNameInMdFiles({pluginName,slug,githubUserName});
                 //Replace slug in pages/admin entry point
                 shell.sed('-i', "wordpress-plugin", slug,  `pages/admin/index.js`); // HERE
                 //Replace name in package.json
                 shell.sed('-i', "pm2-modern-plugin", `${slug}`,  'package.json'); // HERE
+
                 //replace namespace in composer.json
                 shell.sed('-i', originalNamespace, rootNamespace, 'composer.json');
+                shell.sed('-i', originalPluginName, pluginName, 'composer.json');
                 readline.close()
             });
         });
